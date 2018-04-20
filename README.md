4c5d18147e8cde40583e01ed886908bf6e5c3096
Chanan Braunstein <cbraunstein@rosettastone.com>
2018-04-20 17:46:53 -0600

    Container, Row and start of Col

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # BlazorStrap
+
+Port of https://reactstrap.github.io to Blazor
+
+## Done:
+Container
+Row
+
+## In Progress 
+Col
+
+## Not Done
+
+Everything else
--- src/BlazorStrap/content/exampleJsInterop.js
+++
@@ -1,6 +0,0 @@
-﻿// This file is to show how a library package may provide JavaScript interop features
-// wrapped in a .NET API
-
-Blazor.registerFunction('BlazorStrap.ExampleJsInterop.Prompt', function (message) {
-    return prompt(message, 'Type anything here');
-});
