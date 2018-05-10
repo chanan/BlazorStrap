000c44a6bbe21d89afe9589ef2712049bf4ebc09
Chanan Braunstein <cbraunstein@rosettastone.com>
2018-05-09 21:12:48 -0600

    Cards

--- src/BlazorStrap/util/EnumExtensions.cs
+++ src/BlazorStrap/util/EnumExtensions.cs
@@ -33,5 +33,10 @@ namespace BlazorStrap.util
             DescriptionAttribute[] attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
             return attributes.Length > 0 ? attributes[0].Description : string.Empty;
         }
+        public static string ToDescriptionString(this HeadingSize val)
+        {
+            DescriptionAttribute[] attributes = (DescriptionAttribute[])val.GetType().GetField(val.ToString()).GetCustomAttributes(typeof(DescriptionAttribute), false);
+            return attributes.Length > 0 ? attributes[0].Description : string.Empty;
+        }
     }
 }
