b8fda89b018e82f4238d718b24605b4e3bd39899
Maderbacher Ernst <emaderbacher@hotmail.com>
2019-07-04 07:43:27 +0200

    Remove unneeded Space chars from CSS-Classname

---
+++ src/BlazorStrap/util/StringExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace BlazorStrap.util
+{
+	public static class StringExtensions
+	{
+		public static string RemoveUnneededSpaces(this string source)
+		{
+			if (string.IsNullOrEmpty(source))
+				return string.Empty;
+
+			bool bFound = false;
+			StringBuilder sbBuffer = new StringBuilder(source.Length);
+
+			foreach (char chr in source.Trim())
+			{
+				if (chr == ' ')
+				{
+					if (bFound)
+						continue;
+
+					bFound = true;
+				}
+				else
+				{
+					bFound = false;
+				}
+
+				sbBuffer.Append(chr);
+			}
+
+			return sbBuffer.ToString();
+		}
+	}
+}
