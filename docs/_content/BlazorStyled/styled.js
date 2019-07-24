76e041d9cd63a8c2d67cfbe40dd3501285e74782
Chanan Braunstein <cbraunstein@rosettastone.com>
2019-07-24 17:11:25 -0600

    1.0.0-preview7-01

--- docs/_content/BlazorStyled/styled.js
+++ docs/_content/BlazorStyled/styled.js
@@ -1,10 +1,10 @@
-var styleEl = document.createElement('style');
-document.head.appendChild(styleEl);
-var styleSheet = styleEl.sheet;
+var styleEl;
+var styleSheet;
 
 window.styledJsFunctions = {
     insertRule: function (rule, development) {
-        //console.log(rule)
+        //console.log(rule);
+        if (styleEl === undefined) createStylesheet();
         if (development) {
             var text = styleEl.innerText;
             text = text + rule;
@@ -21,5 +21,42 @@ window.styledJsFunctions = {
                 return num;
             }
         }
+    },
+    clearAllRules: function () {
+        const head = document.head;
+        if (styleEl !== undefined) head.removeChild(styleEl);
+        createStylesheet();
+        return true;
+    },
+    addGoogleFont: function (fontlink) {
+        const head = document.head;
+        var found = false;
+        var links = head.getElementsByTagName("link");
+        for (let tag of links) {
+            if (trimHref(tag.href) === trimHref(fontlink)) found = true;
+        }
+        if (!found) {
+            link = document.createElement('link');
+            head.insertBefore(link, document.head.firstChild);
+            link.type = 'text/css';
+            link.rel = 'stylesheet';
+            link.href = fontlink;
+        }
+        return true;
     }
 };
+
+function createStylesheet() {
+    styleEl = document.createElement('style');
+    const head = document.head;
+    if (head.firstChild) {
+        head.insertBefore(styleEl, head.firstChild);
+    } else {
+        head.appendChild(styleEl);
+    }
+    styleSheet = styleEl.sheet;
+}
+
+function trimHref(href) {
+    return href.substring(href.indexOf("//") + 2);
+}
