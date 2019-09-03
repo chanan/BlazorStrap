6b709dc4b094b5a5011d2f70bf26333d58543099
John J Bomhold <jbomhold3@gmail.com>
2019-09-03 11:44:46 -0500

    Redesign of ValidationSummary

--- src/BlazorStrap/Components/Forms/BSValidationSummary.cs
+++ src/BlazorStrap/Components/Forms/BSValidationSummary.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.AspNetCore.Components.RenderTree;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace BlazorStrap.Components.Forms
@@ -10,10 +11,13 @@ namespace BlazorStrap.Components.Forms
     public class BSValidationSummary : ComponentBase
     {
         [CascadingParameter] EditContext CurrentEditContext { get; set; }
-        [Parameter] public bool IsValid { get; set; }
-        [Parameter] public bool IsInvalid { get; set; }
-        [Parameter] public bool Manual { get; set; }
-        [Parameter] public Dictionary<string, string> ErrorMessages { get; set; } = new Dictionary<string, string>();
+        /// <summary>
+        /// string: Key ,  Tuple of string: Error Message, bool: Valid
+        /// </summary>
+        [Parameter] public Dictionary<string, bool> ValidationMessages { get; set; } = new Dictionary<string, bool>();
+        
+        [Parameter(CaptureUnmatchedValues = true)] public IReadOnlyDictionary<string, object> AdditionalAttributes { get; set; }
+
 
         protected override void BuildRenderTree(RenderTreeBuilder builder)
         {
@@ -23,21 +27,36 @@ namespace BlazorStrap.Components.Forms
                 builder.CloseComponent();
             }
 
-            var errors = ErrorMessages.GetEnumerator();
-            if (errors.MoveNext())
+            if (ValidationMessages.Where(q => q.Value == false).Count() > 0)
             {
                 builder.OpenElement(1, "ul");
-                do
+                builder.AddMultipleAttributes(1, AdditionalAttributes);
+                builder.AddAttribute(2, "class", "validation-errors");
+                foreach(var item in ValidationMessages.Where(q => q.Value == false))
                 {
                     builder.OpenElement(2, "li");
                     builder.AddAttribute(3, "class", "validation-message");
-                    builder.AddContent(4, errors.Current);
+                    builder.AddContent(4, item.Key);
                     builder.CloseElement();
 
                 }
-                while (errors.MoveNext());
                 builder.CloseElement();
-            }         
+            }
+            if (ValidationMessages.Where(q => q.Value == true).Count() > 0)
+            {
+                builder.OpenElement(1, "ul");
+                builder.AddMultipleAttributes(1, AdditionalAttributes);
+                builder.AddAttribute(2, "class", "validation-success");
+                foreach (var item in ValidationMessages.Where(q => q.Value == true))
+                {
+                    builder.OpenElement(2, "li");
+                    builder.AddAttribute(3, "class", "validation-message");
+                    builder.AddContent(4, item.Key);
+                    builder.CloseElement();
+
+                }
+                builder.CloseElement();
+            }
         }
     }
 }
