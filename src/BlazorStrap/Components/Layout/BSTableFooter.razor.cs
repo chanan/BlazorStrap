75d81c03023fb9351e80b9a11c2957a36340ac9e
John J Bomhold <jbomhold3@gmail.com>
2019-09-04 12:06:18 -0500

    Fixes oversight

--- src/BlazorStrap/Components/Layout/BSTableFooter.razor.cs
+++ src/BlazorStrap/Components/Layout/BSTableFooter.razor.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 
 namespace BlazorStrap
 {
-    public abstract class BSTableBodyBase : ComponentBase
+    public abstract class BSTableFooterBase : ComponentBase
     {
         [Parameter(CaptureUnmatchedValues = true)] public IDictionary<string, object> UnknownParameters { get; set; }
         [Parameter] public RenderFragment ChildContent { get; set; }
