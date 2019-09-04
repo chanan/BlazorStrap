3eaa3b067aadbea6570164fa58207e7d1ecd339c
John J Bomhold <jbomhold3@gmail.com>
2019-09-04 12:04:36 -0500

    Adds Table Body and Footer

--- src/BlazorStrap/Components/BSCustomizer.razor.cs
+++ src/BlazorStrap/Components/BSCustomizer.razor.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Text;
 
 namespace BlazorStrap
 {
---
+++ src/BlazorStrap/Components/Layout/BSTableBody.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+using BlazorStrap.Util.Components;
+using BlazorComponentUtilities;
+using System;
+using System.Collections.Generic;
+
+namespace BlazorStrap
+{
+    public abstract class BSTableBodyBase : ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)] public IDictionary<string, object> UnknownParameters { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+    }
+}
