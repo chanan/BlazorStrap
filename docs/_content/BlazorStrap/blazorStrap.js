d339f26d05cb3db7e1faae9c3e46edbd9af67b07
John J Bomhold <jbomhold3@gmail.com>
2019-09-23 11:50:21 -0500

    Update Docs

--- docs/_content/BlazorStrap/blazorStrap.js
+++ docs/_content/BlazorStrap/blazorStrap.js
@@ -1,16 +1,93 @@
 ﻿var link;
 
 window.blazorStrap = {
+    animationEvent: function (event) {
+        if (event.target.hasAttributes()) {
+            var name = "";
+            var attrs = event.target.attributes;
+            for (var i = 0; i < attrs.length; ++i) {
+                 name = attrs[i].name;
+                if (name.includes("_bl_")) {
+                    name = name.replace("_bl_", "");
+                    break;
+                }
+            }
+            DotNet.invokeMethodAsync("BlazorStrap", "OnAnimationEnd", name);
+        }
+        else {
+            DotNet.invokeMethodAsync("BlazorStrap", "OnAnimationEnd", event.target.id);
+        }
+    },
     log: function (message) {
         console.log("message: ", message);
         return true;
     },
-    changeBody: function (classname) {
-        document.body.className = classname;
+    addEventAnimationEnd(el) {
+        el.addEventListener('animationend', window.blazorStrap.animationEvent);
+        el.addEventListener('transitionend', window.blazorStrap.animationEvent);
         return true;
     },
-    changeBodyModal: function (padding) {
-        document.body.style.paddingRight = padding;
+    removeEventAnimationEnd(el) {
+        el.removeEventListener('animationend', window.blazorStrap.animationEvent);
+        el.removeEventListener('transitionend', window.blazorStrap.animationEvent);
+        return true;
+    },
+    addClass2Elements: function (el,el2, Classname) {
+        el.classList.add(Classname);
+        el2.classList.add(Classname);
+        return true;
+    },
+    removeClass2Elements: function (el, el2, Classname) {
+        el.classList.remove(Classname);
+        el2.classList.remove(Classname);
+        return true;
+    },
+    addClass: function (el, Classname) {
+        var r = Classname.split(" ");
+        for (var i = 0; i < r.length; ++i) {
+            el.classList.add(r[i]);
+        }
+        DotNet.invokeMethodAsync("BlazorStrap", "OnAddClass", el.id, Classname);
+        return true;
+    },
+    getScrollHeight: function (el) {
+        return el.scrollHeight;
+        
+    },
+    setStyle: function (el, key, value) {
+        el.style[key] = value;
+        return true;
+    },
+    removeClass: function (el, Classname) {
+        var r = Classname.split(" ");
+        for (var i = 0; i < r.length; ++i) {
+            el.classList.remove(r[i]);
+        }
+        return true;
+    },
+    addBodyClass: function (Classname) {
+        if (Classname == "modal-open") {
+            this.changeBodyPaddingRight("17px");
+        }
+        document.body.classList.add(Classname);
+        return true;
+    },
+    removeBodyClass: function (Classname) {
+        if (Classname == "modal-open") {
+            this.changeBodyPaddingRight("");
+        }
+        document.body.classList.remove(Classname);
+        return true;
+    },
+    changeBodyClass: function (Classname) {
+        document.body.className = Classname;
+        return true;
+    },
+    changeBodyPaddingRight: function (padding) {
+        var dpi = window.devicePixelRatio;
+        if (dpi == 1 || !padding) {
+            document.body.style.paddingRight = padding;
+        }
         return true;
     },
     popper: function (target, popperId, arrow, placement) {
@@ -91,3 +168,4 @@ function showPopper(reference, popper, arrow, placement) {
     );
     return thePopper;
 }
+
