7559a24d7fefc02a009fe283417a7676fbf3e8ef
Sören Kuklau <kuklau@trenz.de>
2020-02-26 19:56:15 +0100

    ClientSideSample: modernize Program.cs
    (As per https://devblogs.microsoft.com/aspnet/blazor-webassembly-3-2-0-preview-1-release-now-available/)

--- src/Sample/Program.cs
+++ src/Sample/Program.cs
@@ -1,19 +1,29 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using BlazorPrettyCode;
+using BlazorStrap;
+using BlazorStyled;
+using Microsoft.AspNetCore.Blazor.Hosting;
+using SampleCore;
+using System.Threading.Tasks;
 
 namespace Sample
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-            CreateHostBuilder(args).Build().Run();
-        }
+            builder.Services.AddBlazorPrettyCode(defaults =>
+            {
+                defaults.DefaultTheme = "SolarizedDark";
+                defaults.ShowLineNumbers = true;
+            });
+            builder.Services.AddBootstrapCSS();
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args)
-        {
-            return BlazorWebAssemblyHost.CreateDefaultBuilder()
-.UseBlazorStartup<Startup>();
+            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<ClientSideStyled>("#styled");
+
+            await builder.Build().RunAsync();
         }
     }
 }
