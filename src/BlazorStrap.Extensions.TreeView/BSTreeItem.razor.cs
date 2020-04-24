11277f3968cef36a01b6c19c87a77d49d06777be
John J Bomhold <jbomhold3@gmail.com>
2020-04-24 09:10:15 -0500

    Add TreeView Extension

---
+++ src/BlazorStrap.Extensions.TreeView/BSTreeItem.razor.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace BlazorStrap.Extensions.TreeView
+{
+    public partial class BSTreeItem : ComponentBase
+    {
+        [Parameter] public bool IsActive { get; set; }
+        [Parameter] public string Label { get; set; }
+        [Parameter] public RenderFragment ChildContent { get; set; }
+        [Parameter] public RenderFragment Action { get; set; }
+        [Parameter] public bool Active { get; set; }
+        [Parameter] public EventCallback<MouseEventArgs> OnClick { get; set; }
+        [CascadingParameter] public BSTree Root { get; set; }
+        public BSTreeNode Child { get; set; }
+        [Parameter] public bool IsOpen {
+            get { return _isOpen; }
+            set { _isOpen = value; if(Child != null) Child.IsOpen = value; StateHasChanged(); } 
+        }
+        private bool _isOpen { get; set; }
+
+        protected override void OnInitialized()
+        {
+            if (Root.Expand)
+            {
+                IsOpen = true;
+            }
+        }
+        protected void OnClickEvent(MouseEventArgs e)
+        {
+            IsOpen = !IsOpen;
+            if (OnClick.HasDelegate)
+            {
+                OnClick.InvokeAsync(e);
+            }
+        }
+        public void ChildSet()
+        {
+            StateHasChanged();
+        }
+    }
+}
