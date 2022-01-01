528a48f86b50ac67609dd5e8c9f25712c0e04842
John Bomhold <jbomhold3@gmail.com>
2022-01-01 05:47:40 -0600

    Remove IsCssHover Removes Console.Writeline messages Sort code on BSDropDown

--- src/BlazorStrap/Components/BootstrapComponents/BSDropdown.razor.cs
+++ src/BlazorStrap/Components/BootstrapComponents/BSDropdown.razor.cs
@@ -7,37 +7,28 @@ namespace BlazorStrap
 {
     public partial class BSDropdown : BlazorStrapBase, IAsyncDisposable
     {
-        private DotNetObjectReference<BSDropdown> _objectRef;
-        internal Action<bool, BSDropdownItem>? OnSetActive { get; set; }
         [Parameter] public bool AllowItemClick { get; set; }
         [Parameter] public bool AllowOutsideClick { get; set; }
         [Parameter] public RenderFragment? Content { get; set; }
         [Parameter] public bool Demo { get; set; }
-        [Parameter] public bool IsDiv { get; set; }
         [Parameter] public bool IsDark { get; set; }
+        [Parameter] public bool IsDiv { get; set; }
         [Parameter] public bool IsManual { get; set; }
-        [Parameter] public bool IsStatic { get; set; }
-        [Parameter] public bool IsCssHover { get; set; }
         [Parameter] public bool IsNavPopper { get; set; }
-        private bool _LastIsNavPopper { get; set; }
+        [Parameter] public bool IsStatic { get; set; }
         [Parameter] public string? Offset { get; set; }
+        [Parameter] public Placement Placement { get; set; } = Placement.RightStart;
         [Parameter] public string? ShownAttribute { get; set; }
         [Parameter] public string Target { get; set; } = Guid.NewGuid().ToString();
         [Parameter] public RenderFragment? Toggler { get; set; }
+        private bool _lastIsNavPopper;
+        private DotNetObjectReference<BSDropdown> _objectRef;
+        [CascadingParameter] public BSNavItem? DropdownItem { get; set; }
         [CascadingParameter] public BSButtonGroup? Group { get; set; }
         [CascadingParameter] public BSNavItem? NavItem { get; set; }
-        [CascadingParameter] public BSNavItem? DropdownItem { get; set; }
         [CascadingParameter] public BSDropdown? Parent { get; set; }
-        [Parameter] public Placement Placement { get; set; } = Placement.RightStart;
         internal bool Active { get; private set; }
         internal int ChildCount { get; set; }
-
-        private string? IsDivClassBuilder => new CssBuilder()
-            .AddClass("dropdown", Parent == null)
-            .AddClass("dropup", Placement is Placement.Top or Placement.TopEnd or Placement.TopStart)
-            .AddClass("dropstart", Placement is Placement.Left or Placement.LeftEnd or Placement.LeftStart)
-            .AddClass("dropend", Placement is Placement.Right or Placement.RightEnd or Placement.RightStart)
-            .Build().ToNullString();
 
         private string? ClassBuilder => new CssBuilder("dropdown-menu")
             .AddClass("dropdown-menu-dark", IsDark)
@@ -53,7 +44,15 @@ namespace BlazorStrap
             .AddClass(Class, !string.IsNullOrEmpty(Class))
             .Build().ToNullString();
 
+        private string? IsDivClassBuilder => new CssBuilder()
+            .AddClass("dropdown", Parent == null)
+            .AddClass("dropup", Placement is Placement.Top or Placement.TopEnd or Placement.TopStart)
+            .AddClass("dropstart", Placement is Placement.Left or Placement.LeftEnd or Placement.LeftStart)
+            .AddClass("dropend", Placement is Placement.Right or Placement.RightEnd or Placement.RightStart)
+            .Build().ToNullString();
+
         private ElementReference MyRef { get; set; }
+        internal Action<bool, BSDropdownItem>? OnSetActive { get; set; }
         private BSPopover? PopoverRef { get; set; }
         internal bool Shown { get; private set; }
 
@@ -77,27 +76,31 @@ namespace BlazorStrap
             await InvokeAsync(StateHasChanged);
         }
 
-        protected override void OnParametersSet()
+        public override async Task InteropEventCallback(string id, CallerName name, EventType type)
         {
-            Console.WriteLine(IsNavPopper);
-            if (IsNavPopper == false)
+            if (id == DataId && name.Equals(typeof(ClickForward)) && type == EventType.Click)
             {
-                if (_LastIsNavPopper != IsNavPopper)
-                {
-                    Console.WriteLine("un set");
-                    PopoverRef = null;
-                }
+                await ToggleAsync();
             }
-            else
-            {
-                if (IsNavPopper != _LastIsNavPopper == false)
-                {
-                    Shown = false;
-                    StateHasChanged();
-                }
-            }
+        }
 
-            _LastIsNavPopper = IsNavPopper;
+        [JSInvokable]
+        public override async Task InteropEventCallback(string id, CallerName name, EventType type,
+            Dictionary<string, string>? classList, JavascriptEvent? e)
+        {
+            // The if statement was getting hard to read so split into parts 
+            if (id == DataRefId && name.Equals(this) && type == EventType.Click)
+            {
+                // If this dropdown toggle return
+                if (e?.Target.ClassList.Any(q => q.Value == "dropdown-toggle") == true &&
+                    e.Target.TargetId == DataId) return;
+
+                // If click element is inside this dropdown return
+                // if (e?.Target.ChildrenId?.Any(q => q == DataId) == true && AllowItemClick) return;
+                // If is Manual Return
+                if (IsManual) return;
+                await HideAsync();
+            }
         }
 
         // ReSharper disable once MemberCanBePrivate.Global
@@ -128,43 +131,37 @@ namespace BlazorStrap
             return Shown ? HideAsync() : ShowAsync();
         }
 
-        internal void SetActive(bool active, BSDropdownItem item)
-        {
-            OnSetActive?.Invoke(active, item);
-        }
-
         protected override void OnInitialized()
         {
-            _LastIsNavPopper = IsNavPopper;
+            _lastIsNavPopper = IsNavPopper;
             _objectRef = DotNetObjectReference.Create<BSDropdown>(this);
             BlazorStrap.OnEventForward += InteropEventCallback;
         }
 
-        public override async Task InteropEventCallback(string id, CallerName name, EventType type)
+        protected override void OnParametersSet()
         {
-            if (id == DataId && name.Equals(typeof(ClickForward)) && type == EventType.Click)
+            if (IsNavPopper == false)
             {
-                await ToggleAsync();
+                if (_lastIsNavPopper != IsNavPopper)
+                {
+                    PopoverRef = null;
+                }
             }
+            else
+            {
+                if (IsNavPopper != _lastIsNavPopper == false)
+                {
+                    Shown = false;
+                    StateHasChanged();
+                }
+            }
+
+            _lastIsNavPopper = IsNavPopper;
         }
 
-        [JSInvokable]
-        public override async Task InteropEventCallback(string id, CallerName name, EventType type,
-            Dictionary<string, string>? classList, JavascriptEvent? e)
+        internal void SetActive(bool active, BSDropdownItem item)
         {
-            // The if statement was getting hard to read so split into parts 
-            if (id == DataRefId && name.Equals(this) && type == EventType.Click)
-            {
-                // If this dropdown toggle return
-                if (e?.Target.ClassList.Any(q => q.Value == "dropdown-toggle") == true &&
-                    e.Target.TargetId == DataId) return;
-
-                // If click element is inside this dropdown return
-                // if (e?.Target.ChildrenId?.Any(q => q == DataId) == true && AllowItemClick) return;
-                // If is Manual Return
-                if (IsManual) return;
-                await HideAsync();
-            }
+            OnSetActive?.Invoke(active, item);
         }
 
         public async ValueTask DisposeAsync()
