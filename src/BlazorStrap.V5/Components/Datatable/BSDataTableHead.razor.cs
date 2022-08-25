782c7d5e191ae86effd35e2f1543375c51968ca8
Christian Wulff <doooozer@gmx.net>
2022-08-25 20:28:46 +0200

    Add property FilterSize to BSDataTableHead

--- src/BlazorStrap.V5/Components/Datatable/BSDataTableHead.razor.cs
+++ src/BlazorStrap.V5/Components/Datatable/BSDataTableHead.razor.cs
@@ -1,11 +1,17 @@
 using BlazorComponentUtilities;
 using BlazorStrap.Extensions;
 using BlazorStrap.Shared.Components.Datatable;
+using Microsoft.AspNetCore.Components;
 
 namespace BlazorStrap.V5
 {
     public partial class BSDataTableHead<TValue> : BSDataTableHeadBase<TValue>
     {
+        /// <summary>
+        /// Size of the filter input.
+        /// </summary>
+        [Parameter] public Size FilterSize { get; set; }
+
         protected override string? SortClassBuilder => new CssBuilder()
                 .AddClass("sort-by", Desc == null)
                 .AddClass("sort", Desc == false)
