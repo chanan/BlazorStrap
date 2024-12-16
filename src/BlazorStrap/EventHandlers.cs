ab9b1b61f2b5f345dfa98e4c6d32cad48d8e1e04
John Bomhold <jbomhold3@gmail.com>
2024-12-16 02:35:21 -1000

    Remove on mouseeneter/mouseleave events as .net 7 added them into the framework and causes conflict

--- src/BlazorStrap/EventHandlers.cs
+++ src/BlazorStrap/EventHandlers.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
-using System;
 
 namespace BlazorStrap
 {
-    [EventHandler("onmouseleave", typeof(MouseEventArgs))]
-    [EventHandler("onmouseenter", typeof(MouseEventArgs))]
     [EventHandler("ontransitionend", typeof(EventArgs))]
     [EventHandler("ontransitionrun", typeof(EventArgs))]
     [EventHandler("onanimationend", typeof(EventArgs))]
